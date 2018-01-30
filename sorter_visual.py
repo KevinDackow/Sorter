fd8bcb40722f4f5ba6be950f0e55b9889aecfe15
KevinDackow <Kevin Dackow>
2018-01-30 14:45:17 -0500

    Modifying bubble sort to conform to Python 3 specifications.

--- sorter_visual.py
+++ sorter_visual.py
@@ -1,3 +1,5 @@
+import colorama
+from colorama import Fore, Back, Style
 import sys
 import threading
 import time
@@ -13,19 +15,20 @@ def print_highlighted(l, changed_one, changed_two):
     for x in l:
         # if x is in the right spot.
         if x == changed_one or x == changed_two:
-            print '\033[39m' + str(x) + '\033[1;m'
+            print(Back.BLUE + " " + str(x) + " ", end='')
         else:
             if x == i:
-                print '\033[42m' + str(x) + '\033[1;m',
+                print(Back.GREEN + " " + str(x) + " ", end='')
             else:
-                print '\033[41m' + str(x) + '\033[1;m',
+                print(Back.RED + " " + str(x) + " ", end='')
         i = i + 1
+    print(Style.RESET_ALL)
 
 # helper for seeing if a list is sorted.
 def is_sorted(l): 
-    return all(l[i] <= l[i+1] for i in xrange(len(l)-1))
+    return all(l[i] <= l[i+1] for i in range(len(l)-1))
 
-def bubble_sort(l):
+def bubble_sort(l, wait_time):
     while not is_sorted(l):
         i = 0
         while i < len(l) - 1:
@@ -34,25 +37,30 @@ def bubble_sort(l):
                 lower = int(l[i + 1])
                 l[i + 1] = orig
                 l[i] = lower
-                print_highlighted(l, l[i + 1], l[i]),
+                print_highlighted(l, l[i + 1], l[i])
+                wait(wait_time)
+                print('')
             i = i + 1
+    print_highlighted(l, None, None)
 
 def selection_sort(l):
-
+    1
 
 def main():
     parser = argparse.ArgumentParser(description='Specify type of sorting.')
     parser.add_argument('-ls', dest='list_size', action='store', type=int, default=10, help='integer for the size of the list to sort.')
+    parser.add_argument('-wt', dest='wait_time', action='store', type=float, default=0, help='integer corresponding to time between prints.')
     parser.add_argument('-b', dest='algorithm', action='store_const', const=bubble_sort,  help='bubble sort', default=None)
     args = parser.parse_args()
     if args.list_size <= 0:
-        print 'specified list size <= 0. Using default list size of 10.'
+        print('specified list size <= 0. Using default list size of 10.')
         args.list_size = 10
-    lst = range(1, args.list_size + 1)
+    lst = list(range(1, args.list_size + 1))
     random.shuffle(lst)
     if args.algorithm != None:
-        args.algorithm(lst, args.to_print)
+        args.algorithm(lst, args.wait_time)
     else: 
         print('No specified algorithm. Using bubblesort.')
-        bubble_sort(lst)
+        bubble_sort(lst, args.wait_time)
+
 main()
