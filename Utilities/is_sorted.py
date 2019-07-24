b4fbe42767e0e97068d0e11afe285cd23d7d09cb
KevinDackow <Kevin Dackow>
2019-07-23 21:28:03 -0400

    Removed unecessary files

--- Utilities/is_sorted.py
+++
@@ -1,4 +0,0 @@
-# helper for seeing if a list is sorted.
-def is_sorted(l): 
-    return all(l[i] <= l[i+1] for i in range(len(l)-1))
-
--- Utilities/wait.py
+++
@@ -1,4 +0,0 @@
-import time
-#simple helper function to wait a specified number of seconds
-def wait(n):
-    time.sleep(n)
